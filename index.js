e0e72209ebce0e830eb656e8a3859586f21f26e0
Saiful Islam <sakibbhaiya143@gmail.com>
2023-04-30 22:56:56 +0600

    Done

--- index.js
+++ index.js
@@ -3,12 +3,29 @@ const app = express();
 const port = 5000;
 const cors = require("cors");
 
+const rooms = require("./data/data.json")
+
 app.use(cors());
 
 app.get("/",(req,res)=>{
     res.send("The travel Guru Server");
 })
 
+app.get("/allRooms",(req,res)=>{
+    res.send(rooms);
+})
+
+app.get("/allRooms/:id" , (req,res)=>{
+    const id = req.params.id;
+    const selectedRoom = rooms.filter(room => room.id == id);
+    res.send(selectedRoom);
+})
+
+app.get("/allRooms/:location" , (req, res)=>{
+    const location = req.params.location;
+    const selectedRoom = rooms.filter(room => room.location == location);
+    res.send(selectedRoom);
+})
 app.listen(port,()=>{
     console.log(`This app listening at port: ${port}`);
 })
